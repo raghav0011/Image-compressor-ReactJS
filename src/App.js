080c58abb2041675170e8490c5e7db9ed46cfefd
Raghav GC <110081656+raghav0011@users.noreply.github.com>
2022-12-29 16:22:36 +0545

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, Grid, Image, Item } from "semantic-ui-react";
+import { useState } from "react";
+import "./App.css";
+import imageCompression from "browser-image-compression";
 
 function App() {
+  const [origImage, setOrigImage] = useState("");
+  const [origImageFile, setOrigImageFile] = useState("");
+  const [compressedImage, setCompressedImage] = useState("");
+  const [fileName, setFileImage] = useState("");
+
+  const handle = (e) => {
+    const imageFile = e.target.files[0];
+    setOrigImage(imageFile);
+    setOrigImageFile(URL.createObjectURL(imageFile));
+    setFileImage(imageFile.name);
+  };
+
+  const handleImageCompress = (e) => {
+    e.preventDefault();
+    const options = {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 1920,
+      useWebWorker: true,
+    };
+    if (options.maxSizeMB >= origImage / 1024) {
+      alert("Your image is to big");
+      return 0;
+    }
+    let output;
+    imageCompression(origImage, options).then((x) => {
+      output = x;
+
+      const downloadLink = URL.createObjectURL(output);
+      setCompressedImage(downloadLink);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1></h1>
+      <Container>
+        <Grid>
+          <Grid.Column width={6}>
+            <Item>
+              {origImage ? (
+                <Image src={origImageFile}></Image>
+              ) : (
+                <Image src="http://navparivartan.in/wp-content/uploads/2018/11/placeholder.png"></Image>
+              )}
+            </Item>
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <input
+              type="file"
+              accept="image/*"
+              className="mt-2 btn btn-dark w-75"
+              onChange={(e) => handle(e)}
+            />
+            <h1></h1>
+            {origImage && (
+              <Button
+                primary
+                onClick={(e) => {
+                  handleImageCompress(e);
+                }}>
+                Compress Image
+              </Button>
+            )}
+            <h1></h1>
+            {compressedImage && (
+              <Button>
+                <a href={compressedImage} download={fileName}>
+                  {" "}
+                  Download Image
+                </a>
+              </Button>
+            )}
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <Item>
+              {compressedImage ? (
+                <Image src={compressedImage}></Image>
+              ) : (
+                <Image src="http://navparivartan.in/wp-content/uploads/2018/11/placeholder.png"></Image>
+              )}
+            </Item>
+          </Grid.Column>
+        </Grid>
+      </Container>
     </div>
   );
 }
